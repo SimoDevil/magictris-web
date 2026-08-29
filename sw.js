ffa4ff11c18755eda6fc3a26d7a5b132834c3860
SimoDevil <simonevarie2020@gmail.com>
2026-08-29 14:22:55 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,43 @@
+// Service worker minimale: mette in cache l'app shell così il gioco
+// funziona sempre offline, anche alla primissima apertura senza rete
+// dopo il primo caricamento andato a buon fine.
+const CACHE_NAME = 'magic-tris-v4';
+const APP_SHELL = [
+  './index.html',
+  './style.css',
+  './game.js',
+  './manifest.json',
+  './simo-studio-intro.mp4',
+  './vittoria.mp3',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(APP_SHELL))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== CACHE_NAME).map((k) => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    fetch(event.request)
+      .then((response) => {
+        // aggiorna la cache con la versione fresca appena scaricata
+        const copy = response.clone();
+        caches.open(CACHE_NAME).then((cache) => cache.put(event.request, copy));
+        return response;
+      })
+      .catch(() => caches.match(event.request))
+  );
+});
